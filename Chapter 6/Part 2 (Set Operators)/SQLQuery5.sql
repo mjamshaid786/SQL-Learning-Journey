a2ca0bc29f0122aa391f1ad539c8e0d53cf0c025
Muhammad Jamshaid <bnglagame@gmail.com>
2026-02-24 12:33:06 +0500

    Chapter 6 Part 2 - Learned Set Operator "UNION"
    With detailed explaination of of aliases

---
+++ Chapter 6/Part 2 (Set Operators)/SQLQuery5.sql
@@ -0,0 +1,37 @@
+/*
+Orders are stored in two different tables(Orders & Order Archive)
+Combine all data into single reports without duplicates
+*/
+SELECT 
+'Order' AS SourceTable ,
+	[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.Orders
+
+UNION
+
+SELECT 'OrderArchive' AS SourceTable,
+     [OrderID]
+	,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.OrdersArchive
+ORDER BY OrderID
