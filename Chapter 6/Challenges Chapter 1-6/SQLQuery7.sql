a921c96ad28a37378674ba6b809ff17a2ffb7fcf
Muhammad Jamshaid <bnglagame@gmail.com>
2026-02-26 17:31:42 +0500

    7.  Five Rows Added To Online_Sales Table

---
+++ Chapter 6/Challenges Chapter 1-6/SQLQuery7.sql
@@ -0,0 +1,11 @@
+-- 5 rows add karein Online_Sales Table...
+
+INSERT INTO Online_Sales (SalesID, ProdID, Customer, SaleDate, Qty, Discount_ApplieD)
+VALUES 
+	(01, 1, 'Ali Raza', '2026/02/20', 1, 1),
+	(02, 2, 'Hassan', '2026/02/22', 3, 0),
+	(03, 3, 'Junaid', '2026/02/26', 5, 1),
+	(04, 4, 'Bilal', '2026/02/28', 4, 0),
+	(05, 5, 'Haidar', '2026/02/28', 3, 1)
+
+	select * from Online_Sales
