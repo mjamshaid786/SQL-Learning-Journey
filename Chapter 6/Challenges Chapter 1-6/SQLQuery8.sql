9cf667f592607ad6e529c7d85d420a9079f7060d
Muhammad Jamshaid <bnglagame@gmail.com>
2026-02-26 17:39:35 +0500

    8. Five Rows Are Added Into Store_Sales Table

---
+++ Chapter 6/Challenges Chapter 1-6/SQLQuery8.sql
@@ -0,0 +1,11 @@
+-- 5 rows add karein StoreSales Table mein...
+
+INSERT INTO Store_Sales (SaleID, ProdID, Customer, SaleDate, Qty)
+VALUES
+		(01, 1, 'Ali', '2026/02/23', 2),
+		(02, 2, 'Hassan', '2026/02/25', 1),
+		(03, 3, 'Junaid', '2026/02/22', 4),
+		(04, 4, 'Bilal', '2026/02/27', 3),
+		(05, 5, 'Haidar', '2026/02/28', 7)
+
+select * from Store_Sales
