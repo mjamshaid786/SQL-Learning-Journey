2410b5c95428c1d48b5f432965efbe32f9fc5088
Muhammad Jamshaid <bnglagame@gmail.com>
2026-02-26 17:52:50 +0500

    10. One Row Deleted From StoreSale Table

---
+++ Chapter 6/Challenges Chapter 1-6/SQLQuery10.sql
@@ -0,0 +1,5 @@
+--Delete: Store_Sales se wo records delete karein jo 1 saal se purane hain (ya koi bhi farzi purani date).
+
+select * from Store_Sales
+DELETE FROM Store_Sales
+WHERE SaleDate = '2026-02-28'
