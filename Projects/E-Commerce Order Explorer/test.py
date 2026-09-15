9682e44d008bbe2c91992c3c4031cbd28d1394de
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-15 21:47:31 +0500

    creating new tables for project 8

--- Projects/E-Commerce Order Explorer/test.py
+++ Projects/E-Commerce Order Explorer/test.py
@@ -3,19 +3,15 @@ from connection import get_connection
 
 user_query = """
 --sql
-INSERT INTO customer_orders VALUES
-(5001, 1, 101, 1, '2026-03-01', 'Delivered'),
-(5002, 2, 102, 2, '2026-03-02', 'Pending'),
-(5003, 3, 104, 1, '2026-03-03', 'Delivered'),
-(5004, 1, 103, 2, '2026-03-04', 'Shipped'),
-(5005, 5, 106, 1, '2026-03-05', 'Delivered'),
-(5006, 6, 105, 1, '2026-03-06', 'Pending'),
-(5007, 7, 107, 5, '2026-03-07', 'Delivered'),
-(5008, 8, 101, 1, '2026-03-08', 'Shipped'),
-(5009, 3, 106, 2, '2026-03-09', 'Delivered'),
-(5010, 9, 104, 1, '2026-03-10', 'Cancelled'),
-(5011, 2, 107, 10, '2026-03-11', 'Delivered'),
-(5012, 5, 102, 3, '2026-03-12', 'Pending');
+INSERT INTO drivers VALUES
+(1, 'Ali Raza', 'Lahore'),
+(2, 'Sara Khan', 'Islamabad'),
+(3, 'Hamza Ahmad', 'Faisalabad'),
+(4, 'Ayesha Malik', 'Lahore'),
+(5, 'Bilal Shah', 'Karachi'),
+(6, 'Zainab Noor', 'Islamabad'),
+(7, 'Usman Tariq', 'Faisalabad'),
+(8, 'Hira Ali', 'Lahore');
 """
 try:
     with get_connection() as conn:
